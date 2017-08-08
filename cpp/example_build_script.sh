f3838a08c59932e4a7ea8470949737998ac53766
Gregory R. Lee <gregory.lee@cchmc.org>
2017-08-08 16:11:54 -0400

    fix CMAKE_INSTALL_PREFIX in example build script

--- cpp/example_build_script.sh
+++ cpp/example_build_script.sh
@@ -3,6 +3,6 @@
 
 mkdir -p build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=../bin/Release -DEIGEN_INCLUDE_DIR=/home/lee8rx/anaconda/envs/pyir/include/eigen3 ..
+cmake -DCMAKE_INSTALL_PREFIX=.. -DEIGEN_INCLUDE_DIR=/home/lee8rx/anaconda/envs/pyir/include/eigen3 ..
 make -j4
 make install
